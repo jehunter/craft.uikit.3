15d79a97bba4d33e9ac3ee93ae1324a92422b2c1
Jordan Hunter <jordan@hammerquist.net>
2018-09-06 08:40:11 -0700

    Updating to latest AWS AMI vagrant box

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "mvbcoding/awslinux"
-  config.vm.box_version = "2017.03.0.20170401"
+  config.vm.box = "hbsmith/awslinux"
+  config.vm.box_version = "2018.03.0.20180811"
   config.vm.provision :shell, path: "bootstrap.sh"
   config.vm.network :forwarded_port, guest: 80, host: 4567
-  config.vm.synced_folder ".", "/vagrant", type: "rsync", rsync__auto: true, rsync__exclude: [".vagrant", "Vagrantfile", "bootstrap.sh"]
+  config.vm.synced_folder ".", "/vagrant", type: "rsync", rsync__auto: true, rsync__exclude: [".git", ".vagrant", "Vagrantfile", "bootstrap.sh", "vendor", "node_modules"]
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,24 +8,25 @@ sudo yum update -y
 sudo yum install -y nginx.x86_64
 
 # PHP and necessary modules
-sudo yum install -y php71.x86_64
-sudo yum install -y php71-common.x86_64
-sudo yum install -y php71-fpm.x86_64
-sudo yum install -y php71-mysqlnd.x86_64
-#sudo yum install -y php71-pgsql.x86_64
-sudo yum install -y php71-pdo.x86_64
-sudo yum install -y php71-json.x86_64
-sudo yum install -y php71-pecl-imagick.x86_64
-sudo yum install -y php71-mbstring.x86_64
-#sudo yum install -y php71-mcrypt.x86_64
-sudo yum install -y php71-intl.x86_64
+sudo yum install -y php72.x86_64
+sudo yum install -y php72-common.x86_64
+sudo yum install -y php72-fpm.x86_64
+sudo yum install -y php72-mysqlnd.x86_64
+#sudo yum install -y php72-pgsql.x86_64
+sudo yum install -y php72-pdo.x86_64
+sudo yum install -y php72-json.x86_64
+sudo yum install -y php72-pecl-imagick.x86_64
+sudo yum install -y php72-mbstring.x86_64
+#sudo yum install -y php72-mcrypt.x86_64
+sudo yum install -y php72-intl.x86_64
 
 # MySQL
-sudo yum install -y mysql56-server.x86_64
-sudo yum install -y mysql56.x86_64
+sudo yum install -y mysql57-server.x86_64
+sudo yum install -y mysql57.x86_64
 
 # Postgres SQL
-#sudo yum install -y postgresql95.x86_64
+#sudo yum install -y postgresql96.x86_64
+#sudo yum install -y postgresql96-server.x86_64
 
 # Rsync
 sudo yum install -y rsync.x86_64
@@ -40,6 +41,14 @@ curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
 sudo yum -y install -y nodejs
 sudo npm install npm@latest -g
 
-# Yarn (for UI Kit 3)
-sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
-sudo yum install -y yarn
+cd /vagrant
+
+sudo mv craft.conf /etc/nginx/conf.d/craft.conf
+
+composer install
+
+npm install
+
+sudo service nginx start
+sudo service php-fpm start
+sudo service mysqld start
