5910629b7cceb10c63a81420f29824406966ac4f
Jordan Hunter <jordan@getmercutio.com>
2019-11-18 07:30:49 -0800

    Updates to conform with Craft 3.3.15

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,17 +8,18 @@ sudo yum update -y
 sudo yum install -y nginx.x86_64
 
 # PHP and necessary modules
-sudo yum install -y php72.x86_64
-sudo yum install -y php72-common.x86_64
-sudo yum install -y php72-fpm.x86_64
-sudo yum install -y php72-mysqlnd.x86_64
-#sudo yum install -y php72-pgsql.x86_64
-sudo yum install -y php72-pdo.x86_64
-sudo yum install -y php72-json.x86_64
-sudo yum install -y php72-pecl-imagick.x86_64
-sudo yum install -y php72-mbstring.x86_64
-#sudo yum install -y php72-mcrypt.x86_64
-sudo yum install -y php72-intl.x86_64
+sudo yum install -y php73.x86_64
+sudo yum install -y php73-common.x86_64
+sudo yum install -y php73-fpm.x86_64
+sudo yum install -y php73-mysqlnd.x86_64
+#sudo yum install -y php73-pgsql.x86_64
+sudo yum install -y php73-pdo.x86_64
+sudo yum install -y php73-json.x86_64
+sudo yum install -y php73-pecl-imagick.x86_64
+sudo yum install -y php73-mbstring.x86_64
+#sudo yum install -y php73-mcrypt.x86_64
+sudo yum install -y php73-intl.x86_64
+sudo yum install -y php73-gd.x86_64
 
 # MySQL
 sudo yum install -y mysql57-server.x86_64
@@ -42,18 +43,19 @@ sudo yum -y install -y nodejs
 sudo npm install npm@latest -g
 npm install --global gulp-cli
 
+# Yarn
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum -y install yarn
+
 cd /vagrant
 
 sudo mv craft.conf /etc/nginx/conf.d/craft.conf
 
 composer install
 
-npm install
+#npm install
 # Workaround for node-sass unable to install via sudo
-sudo npm install --unsafe-perm gulp-sass --save-dev
-
-sudo cp node_modules/uikit/dist/js/uikit.min.js web/js/uikit.min.js
-sudo cp node_modules/uikit/dist/js/uikit-icons.min.js web/js/uikit-icons.min.js
+#sudo npm install --unsafe-perm gulp-sass --save-dev
 
 sudo service nginx start
 sudo service php-fpm start
